99229f8603a8749848ad47c95162f98d6022ff6f
SoyAldo <pereyraosvaldo@live.com.ar>
2022-10-11 11:47:48 -0300

    Inventory api test plugin created

---
+++ inventory-api-testing/src/main/java/org/kayteam/inventoryapitesting/InventoryApiTesting.java
@@ -0,0 +1,21 @@
+package org.kayteam.inventoryapitesting;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class InventoryApiTesting extends JavaPlugin {
+
+    private FileConfiguration config;
+    //private final InventoryManager inventoryManager = new InventoryManager( this );
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
